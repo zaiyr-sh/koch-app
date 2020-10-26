eff41ca7f966d2bf04836d382b385bdc9a593d3b
Zaiyr <zaiyr.00@gmail.com>
2020-10-27 00:45:39 +0600

    Added filter section

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ const App = () => {
                 </div>
 
                 <div className="filter-direction">
-                <p className="filter-direction-title">Откуда</p>
+                <p className="filter-title">Откуда</p>
                 <div className="filter-direction-from">
                   <div className="filter-direction-area">
                     <select className="filter-direction-selection" name="area">
@@ -50,7 +50,7 @@ const App = () => {
                   <div className="filter-date">25 сен 2020</div>
                 </div>
 
-                <p className="filter-direction-title">Куда</p>
+                <p className="filter-title">Куда</p>
                 <div className="filter-direction-to">
                   <div className="filter-direction-area">
                     <select className="filter-direction-selection" name="area">
@@ -75,8 +75,9 @@ const App = () => {
 
                 <div className="filter-size">
                   <div className="filter-size-information">
+
                     <div className="filter-weight">
-                      <p className="filter-direction-title">Вес груза, т</p>
+                      <p className="filter-title">Вес груза, т</p>
                       <div className="filter-size-from">
                         <div className="filter-direction-area">
                           <select className="filter-size-selection" name="area">
@@ -98,7 +99,7 @@ const App = () => {
                     </div>
 
                     <div className="filter-volume">
-                      <p className="filter-direction-title">Объем груза, м³ </p>
+                      <p className="filter-title">Объем груза, м³ </p>
                       <div className="filter-size-to">
                         <div className="filter-direction-area">
                           <select className="filter-size-selection" name="area">
@@ -119,10 +120,29 @@ const App = () => {
                       </div>
                     </div>
 
-
+                      <div className="filter-price">
+                          <div className="filter-size-to">
+                              <div className="filter-direction-area">
+                                  <input className="filter-size-selection" type="text" placeholder="Цена от, сом"/>
+                              </div>
+                              <div className="filter-direction-city">
+                                  <input className="filter-size-selection" type="text" placeholder="До"/>
+                              </div>
+                          </div>
+                      </div>
 
                   </div>
                 </div>
+                  {/*Filter size*/}
+
+                 <div className="filter-buttons">
+                     <div className="filter-reset">
+                         <a href="/" className="filter-reset-btn">Сбросить <i className="fa fa-close"/></a>
+                     </div>
+                     <div className="filter-show">
+                         <a href="/" className="filter-show-btn">Показать 941 объявлений</a>
+                     </div>
+                 </div>
 
 
 
