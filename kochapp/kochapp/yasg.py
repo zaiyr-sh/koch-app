3bed41707e069e3087742cbf0ae78b4cf357ba8f
Daniiar <daniiarz.test@gmail.com>
2020-11-07 16:04:56 +0600

    Removed signs from urls

--- kochapp/kochapp/yasg.py
+++ kochapp/kochapp/yasg.py
@@ -16,5 +16,5 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-   path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
