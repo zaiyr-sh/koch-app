69ce6d884606d195dbadb1b65a4a950df63e843a
Daniiar <daniiarz.test@gmail.com>
2020-11-07 14:48:10 +0600

    Added swagger docs

--- kochapp/kochapp/urls.py
+++ kochapp/kochapp/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .yasg import urlpatterns as doc_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/cargo/', include('transportations.urls')),
     path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.authtoken'))
+    path('api/auth/', include('djoser.urls.authtoken')),
+
 ]
+
+urlpatterns += doc_urls
