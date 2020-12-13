a46a9cd8dd87f46376cd26b66e0644d76c39f1ae
Zaiyr <zaiyr.00@gmail.com>
2020-12-14 00:29:17 +0600

    build folder

---
+++ client/build/precache-manifest.377ec7f382df2ee6f5fcd94c604ea52c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30835fa976a243df5bdb3236241e64d7",
+    "url": "/koch-app/index.html"
+  },
+  {
+    "revision": "89fee945b00b24e6e94c",
+    "url": "/koch-app/static/css/main.5cbc0729.chunk.css"
+  },
+  {
+    "revision": "e1975389ad54a69461db",
+    "url": "/koch-app/static/js/2.9026c885.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/koch-app/static/js/2.9026c885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89fee945b00b24e6e94c",
+    "url": "/koch-app/static/js/main.068996f4.chunk.js"
+  },
+  {
+    "revision": "ae7e16dadaea66b13423",
+    "url": "/koch-app/static/js/runtime-main.cbfdfafd.js"
+  },
+  {
+    "revision": "b381c2abd2972024a6a7e3d882208d9b",
+    "url": "/koch-app/static/media/Gilroy-Bold.b381c2ab.ttf"
+  },
+  {
+    "revision": "b487bfc69e2a1cb0578fe2a910da8b2b",
+    "url": "/koch-app/static/media/Gilroy-ExtraBold.b487bfc6.ttf"
+  },
+  {
+    "revision": "c83281ae1ca703d0741a770ee7e7c091",
+    "url": "/koch-app/static/media/Gilroy-Medium.c83281ae.ttf"
+  },
+  {
+    "revision": "a5cf732b15078843b237bd58f3ed44cd",
+    "url": "/koch-app/static/media/Gilroy-SemiBold.a5cf732b.ttf"
+  }
+]);
